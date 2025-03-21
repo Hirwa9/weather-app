916158d854f7d5e76006c05530c044791191ddc2
hirwa9 <hirwawilly9@gmail.com>
2025-03-21 22:27:05 +0200

    Keep the input's background color and color during webkit-autofill

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -15,10 +15,10 @@ const SearchBar = ({ search = () => { }, className }) => {
 
     return (
         <form action="" onSubmit={handleSubmit} className={className}>
-            <div className='flex items-stretch bg-gray-800 text-amber-50 rounded-full overflow-hidden'>
+            <div className='flex items-stretch bg-gray-800 text-amber-50 rounded-full overflow-hidden focus-within:border focus-within:border-dashed focus-within:border-slate-400 '>
                 <MapPin size={20} className='self-center ms-3' />
                 <input
-                    className='flex-1 px-2 outline-0'
+                    className='flex-1 px-2 outline-0 bg-inherit search-input caret-inherit'
                     style={{ transition: "padding .1s" }}
                     type="text"
                     name="city_name"
